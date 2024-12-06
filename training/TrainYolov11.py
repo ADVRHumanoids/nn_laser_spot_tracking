b4acb5d333444c0059de0352497d312b16273a6a
Davide Torielli <toridebraus@gmail.com>
2024-12-06 18:09:53 +0100

    it seems to work

--- training/TrainYolov11.py
+++ training/TrainYolov11.py
@@ -1,15 +1,15 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n-seg.pt")
+model = YOLO("yolo11m-seg.pt")
 
 # Train the model
 train_results = model.train(
-    data="/home/tori/YOLO/data/prova_1733/data.yaml",  # path to dataset YAML
-    epochs=5,  # number of training epochs
-    imgsz=1280,  # training image size
+    data="/home/tori/ws/src/nn_laser_spot_tracking/training/coco_subset_1733499037/data.yaml",  # path to dataset YAML
+    epochs=500,  # number of training epochs
+    imgsz=640,  # training image size
     device="0",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
-    batch = 2,
+    batch = 16,
 )
 
 # Evaluate model performance on the validation set
