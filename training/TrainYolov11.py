57016442f2aa547717e33e2c735cc281a690742c
Davide Torielli <toridebraus@gmail.com>
2024-12-06 12:35:50 +0100

    towards

--- training/TrainYolov11.py
+++ training/TrainYolov11.py
@@ -5,8 +5,8 @@ model = YOLO("yolo11n-seg.pt")
 
 # Train the model
 train_results = model.train(
-    data="/home/tori/YOLO/data/combined_dataset/combined_dataset.yaml",  # path to dataset YAML
-    epochs=100,  # number of training epochs
+    data="/home/tori/YOLO/data/prova_1733/data.yaml",  # path to dataset YAML
+    epochs=5,  # number of training epochs
     imgsz=1280,  # training image size
     device="0",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
     batch = 2,
